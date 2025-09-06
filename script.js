97e2b23e8cf19e8c8b0dc573db7802020144d396
JUJU-MAKER-29 <julienjacopin@gmail.com>
2025-09-06 08:31:15 +0200

    Mon Premier Site

---
+++ script.js
@@ -0,0 +1,10 @@
+function changerTexte() {
+  const message = document.getElementById("message");
+  message.textContent = "Bravo 🎉 tu as cliqué sur le bouton !";
+}
+
+
+function changerTexte2() {
+  const message = document.getElementById("message");
+  message2.textContent = "Bonjour simplicode";
+}
