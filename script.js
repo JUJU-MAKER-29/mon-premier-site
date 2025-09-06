2a2588cd9204a56b8370614b904647a0a8ba2c10
JUJU-MAKER-29 <julienjacopin@gmail.com>
2025-09-06 09:11:59 +0200

    sauvegarde

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function changerTexte() {
   message.textContent = "Bravo 🎉 tu as cliqué sur le bouton !";
 }
 
+function envoyerMessage() {
+  alert("Merci ! Votre message a été envoyé 🚀");
+}
+
 // Burger menu mobile
 const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
 burger.addEventListener('click', () => {
   const ul = document.querySelector('nav ul');
   ul.classList.toggle('active');
